12d9823b7c62b041ec533219ddb5bdf43c9c8d30
Klemensmandzaro <kamil.klemiato@wp.pl>
2023-12-01 10:49:42 +0100

    git

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a[4][4];
+    for(int i=0; i<4; i++)
+    {
+        for(int j=0; j<4; j++)
+        {
+            cin>>a[i][j];
+        }
+    }
+    cout<<a[0][0]+a[1][1]+a[2][2]+a[3][3]+a[0][3]+a[1][2]+a[2][1]+a[3][0];
+    return 0;
+}
